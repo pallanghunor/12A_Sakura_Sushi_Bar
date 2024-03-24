fa29c3ebfc22cf7546a4b9f1a21164f814a23d21
pallanghunor <pallang.hunor@students.jedlik.eu>
2024-03-24 20:49:23 +0100

    home section

--- public/script.js
+++ public/script.js
@@ -10,9 +10,13 @@ var _a, _b;
     (_a = document.querySelector(".login-popup")) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
 });
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-// const scrollHeader = () => {
-//     const header = document.querySelector("#header");
-//     if (this.scrollY >= 50) header?.classList.add("scroll-header");
-//     else header?.classList.remove("scroll-header");
-// };
-// window.addEventListener("scroll", scrollHeader);
+const scrollHeader = () => {
+    console.log("scroll");
+    console.log(window.scrollY);
+    const header = document.querySelector(".navbar");
+    if (window.scrollY >= 50)
+        header === null || header === void 0 ? void 0 : header.classList.add("bg-header");
+    else
+        header === null || header === void 0 ? void 0 : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
--- src/script.ts
+++ src/script.ts
@@ -7,9 +7,11 @@ document.querySelector(".login-popup .close-btn")?.addEventListener("click", () 
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-// const scrollHeader = () => {
-//     const header = document.querySelector("#header");
-//     if (this.scrollY >= 50) header?.classList.add("scroll-header");
-//     else header?.classList.remove("scroll-header");
-// };
-// window.addEventListener("scroll", scrollHeader);
+const scrollHeader = () => {
+    console.log("scroll");
+    console.log(window.scrollY);
+    const header = document.querySelector(".navbar");
+    if (window.scrollY >= 50) header?.classList.add("bg-header");
+    else header?.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
